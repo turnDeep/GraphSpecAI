Fix CUDA assert error in loss calculation
- Added `ignore_index=UNKNOWN_ATOM_INDEX_TARGET` to `F.cross_entropy`
  for `node_types_loss` in `train_supervised` and `evaluate` methods
  of the `SelfGrowingTrainer` class.
- This prevents out-of-bounds target indices for unknown/padding atom
  types from causing errors during loss computation.

gsai0527.py
@@ -1771,7 +1771,8 @@ class SelfGrowingTrainer:
                         predicted_node_types_permuted = pred_node_types.permute(0, 2, 1)
                         node_types_loss = F.cross_entropy(
                             predicted_node_types_permuted,
-                            structural_targets['node_types']
+                            structural_targets['node_types'],
+                            ignore_index=UNKNOWN_ATOM_INDEX_TARGET
                         )
                         loss_p2s = node_exists_loss + node_types_loss
                     else:
@@ -2240,7 +2241,8 @@ class SelfGrowingTrainer:
                         predicted_node_types_permuted = pred_node_types.permute(0, 2, 1)
                         node_types_loss = F.cross_entropy(
                             predicted_node_types_permuted,
-                            structural_targets['node_types']
+                            structural_targets['node_types'],
+                            ignore_index=UNKNOWN_ATOM_INDEX_TARGET
                         )
                         
                         loss_p2s = node_exists_loss + node_types_loss